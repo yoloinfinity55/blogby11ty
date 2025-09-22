b9fa24c06d9ffc7f3bb118c2228368e4815da1de
yoloinfinity55 <yoloinfinity55@gmail.com>
2025-09-22 10:57:59 -0400

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
 	title: "Blog by 11ty",
-	url: "https://example.com/",
+	url: "https://yoloinfinity55.github.io/blogby11ty/",
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Yolo Infinity",
+		email: "yoloinfinity55@gmail.com",
+		url: "https://yoloinfinity55.github.io/blogby11ty/about-me/"
 	}
 }
